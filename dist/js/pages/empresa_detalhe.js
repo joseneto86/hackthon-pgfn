35fc59d30902151f0c73075d7888bf94df79284f
Jose Neto <jose.neto86@gmail.com>
2020-11-23 01:58:09 -0300

    atualizacao

--- dist/js/pages/empresa_detalhe.js
+++ dist/js/pages/empresa_detalhe.js
@@ -8,22 +8,25 @@ function preencherEmpresa(){
 	$("#numeroProcesso").html(processo.id_imoveis);
 	$("#nomeEmpresa").html(processo.nome);
 	$("#endereco").html(processo.street);
-	$("#tipoImovel").html(processo.businesstype);
+	
 	$("#valorDeclarado").html(numeral(processo.declared_price).format('$ 0,0.00'));
 	$("#valorCalculado").html(numeral(processo.price).format('$ 0,0.00'));
 	$("#valorMedioMetroQuadrado").html(numeral(processo.price_m2).format('$ 0,0.00'));
-	$("#dataCadastro").html(processo.createdat);
-	$("#dataPrescricao").html(processo.final_date);
-	if(processo.businesstype == "Residencial"){
+	$("#dataCadastro").html(formatarData(processo.createdat));
+	$("#valorDivida").html(numeral(processo.divida).format('$ 0,0.00'));
+	if(processo.businesstype == "Residencial" || processo.businesstype == "RESIDENTIAL"){
+		$("#tipoImovel").html("Residencial");
 		$("#infoResidencial").css("display", "");
 		$("#infoComercial").css("display", "none");
-		$("#tamanhoImovel").html(processo.totalareas+"m²");
+		$("#tamanhoImovel").html(processo.usableareas+"m²");
 		$("#quartos").html(processo.bedrooms+" quartos");
+		$("#suites").html(processo.suites+" suites");
 		$("#banheiros").html(processo.bathrooms+" banheiros");
 		$("#vagas").html(processo.parkingspaces+" vagas");
 	}else{
 		$("#infoResidencial").css("display", "none");
 		$("#infoComercial").css("display", "");
+		$("#tipoImovel").html("Comercial");
 	}
 
 	$("#divProcesso").on("click", function(){
@@ -42,4 +45,23 @@ function finalizarMapa(){
 	$('#modal-load').modal('hide');
     carregarInfoMapa(processo);
     plotar(mapa);
+}
+
+function formatarData(data){
+	var today = new Date(data);
+	var dd = today.getDate();
+
+	var mm = today.getMonth()+1; 
+	var yyyy = today.getFullYear();
+	if(dd<10) 
+	{
+	    dd='0'+dd;
+	} 
+
+	if(mm<10) 
+	{
+	    mm='0'+mm;
+	} 
+
+	return dd+'/'+mm+'/'+yyyy;
 }
